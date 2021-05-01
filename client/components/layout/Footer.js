fd3aa58399267082e153f63139c0f54f4ff05bad
Salem Feyzo <f1989salim@gmail.com>
2021-05-01 12:36:06 +0300

    package update

--- client/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className='flex place-items-center justify-center py-7'>
-      Copyright 2021 Social Media App
-    </footer>
-  )
-}
-
-export default Footer
